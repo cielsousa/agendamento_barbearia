b5a8a6f307e31384c46c64c1cedaab74acc3e85b
Luciel Nascimento <lsciel67@gmail.com>
2024-11-27 19:26:05 -0300

    modifications on app settings

--- barbearia/barbearia/settings.py
+++ barbearia/barbearia/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("barbearia/secret_key.txt") as f:
-    SECRET_KEY = f.read().strip()
+key = get_random_secret_key()
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
