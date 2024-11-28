ea5b406dca77915028736b8ca18bfdf75f2b641a
Luciel Nascimento <lsciel67@gmail.com>
2024-11-28 15:49:53 -0300

    Testing the host

--- barbearia/barbearia/settings.py
+++ barbearia/barbearia/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+#STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
