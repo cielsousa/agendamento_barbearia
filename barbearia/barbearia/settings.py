562176a86037bc04539761d025a88cda04ebb335
Luciel Nascimento <lsciel67@gmail.com>
2024-12-18 15:43:29 -0300

    testing the host

--- barbearia/barbearia/settings.py
+++ barbearia/barbearia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lucielnascimento.pythonanywhere.com']
 
 
 # Application definition
