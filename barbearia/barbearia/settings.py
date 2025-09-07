cc7776d41a04c82d477b4a87a46689e2630a9d15
Luciel Nascimento <70718683+cielsousa@users.noreply.github.com>
2025-09-06 23:00:21 -0300

    Update settings.py

--- barbearia/barbearia/settings.py
+++ barbearia/barbearia/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['LucielNascimento.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
